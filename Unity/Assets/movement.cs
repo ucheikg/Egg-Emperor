f218a9671f713cd879628bd1033c378a0c4d83cf
UCHEOMA EMELONYE <UE315957@falmouth.ac.uk>
2025-04-29 10:54:35 +0100

    working on making the throttle better

--- Unity/Assets/movement.cs
+++ Unity/Assets/movement.cs
@@ -6,12 +6,15 @@ using GamesAcademy.SerialPackage;
 public class movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform Player;
+    public Transform Playercamera;
+    [SerializeField] private GameObject Player;
+
     private float rotationSpeed = 50f;
+    [SerializeField] private GameObject Loading;
 
     void Start()
     {
-        
+        StartCoroutine(Delay());
     }
 
     // Update is called once per frame
@@ -19,12 +22,38 @@ public class movement : MonoBehaviour
     {
         if (SerialComManager.instance.GetDataFromArduino("s") == "1")
         {
-            Player.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+            Playercamera.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
         }
 
         if (SerialComManager.instance.GetDataFromArduino("d") == "1")
         {
-            Player.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+            Playercamera.Rotate(0, rotationSpeed * Time.deltaTime, 0);
         }
+        
+        
+        
+        if (SerialComManager.instance.GetDataFromArduino("h") == "1")
+        {
+            Player.transform.position = Vector3.forward(Player.transform * 10f * )   
+        }
+        
+        if (SerialComManager.instance.GetDataFromArduino("n") == "1")
+        {
+
+        }
+        
+        if (SerialComManager.instance.GetDataFromArduino("f") == "1")
+        {
+
+        }
+    }
+
+    IEnumerator Delay()
+    {
+        Loading.SetActive(true);
+
+        yield return new WaitForSeconds(30f);
+        
+        Loading.SetActive(false);
     }
 }
