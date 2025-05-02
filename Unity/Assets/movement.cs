172931d119193ee613c75895633f40682f4354f0
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-05-02 14:25:32 +0100

    Added comments to my code

--- Unity/Assets/movement.cs
+++ Unity/Assets/movement.cs
@@ -10,28 +10,29 @@ public class movement : MonoBehaviour
     [SerializeField] private GameObject Player;
 
     private float rotationSpeed = 50f;
-    [SerializeField] private GameObject Loading;
 
     void Start()
     {
-        StartCoroutine(Delay());
+    
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if the arduino send over 1 then the player should turn to the left
         if (SerialComManager.instance.GetDataFromArduino("s") == "1")
         {
             Playercamera.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
         }
-
+        //if the arduino send over 1 then the player should turn to the right
         if (SerialComManager.instance.GetDataFromArduino("d") == "1")
         {
             Playercamera.Rotate(0, rotationSpeed * Time.deltaTime, 0);
         }
         
         
-        
+        //if the arduino send over 1 then the player should move foward according to the speed set
+        // the speed is decided by which boolean is true in the aurdino code as each if statee=ment only applies to its respective boolean that dont exist at the same time;
         if (SerialComManager.instance.GetDataFromArduino("h") == "1")
         {
             Player.transform.Translate(Vector3.forward * 15f * Time.deltaTime);
@@ -46,14 +47,5 @@ public class movement : MonoBehaviour
         {
             Player.transform.Translate(Vector3.forward * 50f * Time.deltaTime);
         }
-    }
-
-    IEnumerator Delay()
-    {
-        Loading.SetActive(true);
-
-        yield return new WaitForSeconds(30f);
-        
-        Loading.SetActive(false);
     }
 }
