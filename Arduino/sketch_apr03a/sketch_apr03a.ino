79d48988179cc8f49fa6fe923a7e0f1a53b9a6d1
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-05-02 14:13:04 +0100

    added in comments to code for better understanding

--- Arduino/sketch_apr03a/sketch_apr03a.ino
+++ Arduino/sketch_apr03a/sketch_apr03a.ino
@@ -1,9 +1,11 @@
+// setting the Pins to be read
 const int piezoPin = A0;
 const int throttle = A1;
 const int punchingBag = 7;
 const int steeringWheel = A2;
 const int turret = 8;
 
+// steering wheel values
 int rightTurn = 341;
 int middleValue = 681;
 int leftTurn = 1023;
@@ -11,6 +13,7 @@ int steeringValue = 0;
 bool left;
 bool right;
 
+// throttle values
 int throttlevalue = 0;
 int Stop = 0;
 int slow = 341;
@@ -20,6 +23,7 @@ bool point1;
 bool point2;
 bool point3;
 
+// voice module value with threshold
 int piezoValue = 0;
 int threshold = 700;
 bool screaming = false;
@@ -32,7 +36,7 @@ void setup() {
   pinMode(turret, INPUT_PULLUP);
 
   bool online = false;
-
+// checks if a connection is possible using handhsake x
   while (!online) {
     while (!Serial.available()) {
     }
@@ -47,47 +51,55 @@ void setup() {
 }
 
 void loop() {
+  // set values to each of their respective categories;
   piezoValue = analogRead(piezoPin);
   throttlevalue = analogRead(throttle);
   steeringValue = analogRead(steeringWheel);
 
+  // checks if player screams loud enough for the value to pass the thresshold
+  // if yes then it will make screaming true
   if (piezoValue > threshold) {
     screaming = true;
   } else {
     screaming = false;
   }
 
+  // gives a bull output for unity to read
+
+  // bool outputs depend on on the direction the potentiometer has been turned 1023 being its max turn and 0 being it minimun turn 
+  //checks if it is turned to the right
   if (steeringValue < rightTurn) {
     right = true;
     left = false;
   }
-
+// checks if it is in the middle
   if (rightTurn < steeringValue && steeringValue < middleValue) {
     right = false;
     left = false;
   }
-
+// checks if its turned to the left
   if (middleValue < steeringValue && steeringValue <= leftTurn) {
     left = true;
     right = false;
   }
-
+// checks if its been turned all the way back
   if (throttlevalue = Stop) {
     point1 = false;
     point2 = false;
     point3 = false;
   }
-
+// checks if it has been turned slightly foward
   if (Stop < throttlevalue && throttlevalue < slow) {
     point1 = true;
     point2 = false;
     point3 = false;
   }
+  // checks if it is in the middle
   if (slow < throttlevalue && throttlevalue < medium) {
     point1 = false;
     point2 = true;
     point3 = false;
-
+// checks if it is fully pushed foward
   }
   if (medium < throttlevalue && throttlevalue  <= fast) {
     point1 = false;
@@ -95,7 +107,7 @@ void loop() {
     point3 = true;
 
   }
-
+// sneds a charcter to unity so it knows which data to read from;
   if (!Serial.available()) {
     return;
   }
