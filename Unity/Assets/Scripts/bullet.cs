b2f62649f3a7cb921298661bb808adf6a67eca6f
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-05-09 14:40:56 +0100

    added in comments to code for better understanding

--- Unity/Assets/Scripts/bullet.cs
+++ Unity/Assets/Scripts/bullet.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class bullet : MonoBehaviour
 {
 
-
+    // if bullet makes contact with enemy or the ground then it should be destroyed;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("ground"))
