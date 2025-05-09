9db44b9f8d852969454edcc3f19c359893108a2d
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-05-09 14:37:12 +0100

    added in comments to code for better understanding

--- Unity/Assets/Scripts/Knux_Movement.cs
+++ Unity/Assets/Scripts/Knux_Movement.cs
@@ -10,12 +10,15 @@ public class Knux_Movement : MonoBehaviour
     public Transform player;
     private void Start()
     {
+        // assigns a gameobject to player by a specific name
         player = GameObject.Find("Player").transform;
+        // agents uses navmesh as navigation
         agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
+        // agent will chase player down
         agent.SetDestination(player.position);
     }
 
