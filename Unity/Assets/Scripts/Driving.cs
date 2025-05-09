fb87aae0ba1d39c0075ea4ba8d26691955e1f516
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-05-09 14:32:36 +0100

    added in comments to code for better understanding

--- Unity/Assets/Scripts/Driving.cs
+++ Unity/Assets/Scripts/Driving.cs
@@ -11,6 +11,7 @@ public class Driving : MonoBehaviour
 
     void Start()
     {
+        // Stops all courtines so they do not clash;
         StopCoroutine(Slow());
         StopCoroutine(Breaks());
         StopCoroutine(Medium());
@@ -22,28 +23,28 @@ public class Driving : MonoBehaviour
     {
         //if the arduino send over 1 then the player should move foward according to the speed set
         // the speed is decided by which boolean is true in the aurdino code as each if statee=ment only applies to its respective boolean that dont exist at the same time;
-
+        // starts break
         if (SerialComManager.instance.GetDataFromArduino("o") == "1")
         {
             StartCoroutine(Breaks());
         }
-
+        // starts first speed range
         if (SerialComManager.instance.GetDataFromArduino("h") == "1")
         {
             StartCoroutine(Slow());
         }
-
+        // starts second speed range
         else if (SerialComManager.instance.GetDataFromArduino("n") == "1")
         {
             StartCoroutine(Medium());
         }
-
+        // starts third speed range
         else if (SerialComManager.instance.GetDataFromArduino("f") == "1")
         {
             StartCoroutine(Fast());
         }
     }
-
+    // stops player movement
     IEnumerator Breaks()
     {
         Player.transform.Translate(Vector3.forward * 0f * Time.deltaTime);
@@ -52,7 +53,7 @@ public class Driving : MonoBehaviour
             yield break;
         }
     }
-
+    // player has slow movement
     IEnumerator Slow()
     {
         Player.transform.Translate(Vector3.forward * 25f * Time.deltaTime);
@@ -61,7 +62,7 @@ public class Driving : MonoBehaviour
             yield break;
         }
     }
-
+    // player has medium movement
     IEnumerator Medium()
     {
         Player.transform.Translate(Vector3.forward * 50f * Time.deltaTime);
@@ -70,7 +71,7 @@ public class Driving : MonoBehaviour
             yield break;
         }
     }
-
+    // player mhas fast movement
     IEnumerator Fast()
     {
         Player.transform.Translate(Vector3.forward * 100f * Time.deltaTime);
