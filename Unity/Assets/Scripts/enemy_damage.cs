cb6ef3c70337f59006ea9e15293dfa2fd4290cbc
UCHEOMA EMELONYE <ue315957@falmouth.ac.uk>
2025-05-09 14:45:29 +0100

    added in comments to code for better understanding

--- Unity/Assets/Scripts/enemy_damage.cs
+++ Unity/Assets/Scripts/enemy_damage.cs
@@ -11,6 +11,8 @@ public class enemy_damage : MonoBehaviour
 
     void Update()
     {
+
+        // if enemies health is less than or equal to zero then you win and game quits
         if (health <= 0)
         {
             Destroy(gameObject);
@@ -19,9 +21,10 @@ public class enemy_damage : MonoBehaviour
         }
 
     }
-
+    // if enemy maes contact with enemy of the weapon systems then enemy takes damage
     private void OnTriggerEnter(Collider other)
     {
+        // laser does consistent damage to gameobject
         if (other.gameObject.CompareTag("laser"))
         {
             currentHealth = health;
@@ -39,6 +42,7 @@ public class enemy_damage : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+    //when the gameobject leaves the lasers range it should stop taking damage
         if (other.gameObject.CompareTag("laser"))
         {
             health = currentHealth;
